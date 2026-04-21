1fa55f5df801bfd3c31ed1461c6e2476ae702db2
Allison K <amklinglerr@gmail.com>
2026-04-21 16:29:30 -0400

    mcts outline updated

--- chess_engine/mcts.py
+++ chess_engine/mcts.py
@@ -7,18 +7,33 @@ from chess import Move
 
 class State():
 
-    def __init__(self, value=0, moves = []):
+    def __init__(self, value=0, board, player):
         self.value = value
-        self.moves=moves
+        self.moves = board.legal_moves
+        self.board = board
+        self.player = player # this is whether we are black or white
         # something to keep track of whether it is this agents turn or not?
 
     def is_terminal(self):
+        if board.is_checkmate():
+            return True
+        if board.is_stalemate():
+            return True
         if len(self.moves) == 0:
             return True
         return False
 
     def reward(self):
-        # tbd
+        if board.is_stalemate():
+            return 50
+        if board.is_checkmate():
+            if board.outcome().winner = self.player:
+                return 100
+            else:
+                return 0
+            # determine which player won the game? agent or opponent...
+        
+        # the rest tbd for the recursive steps i sppose
 
     def next_state(self, move):
         # tbd
@@ -32,6 +47,7 @@ class Node():
         self.state = state
         self.children = []
         self.parent = parent
+        self.untried_actions = self.state.moves # need to copy this because we will be adding/removing?
     
     def add_child(self, child_state):
         child = Node(child_State, self)
@@ -41,7 +57,17 @@ class Node():
         self.reward += reward
 
     def is_expanded(self):
-        # tbd
+        if len(self.untried_actions) == 0:
+            return True
+        return False
 
-def tree_policy():
-    # tbd
+    def rollout(self):
+        # based on rollout policy -> tbd
+
+    def backpropagate(self):
+        # beep
+
+    def best_child(self):
+        # beep boop
+
+    # non-class based rollout policy and tree policy or do those go in here to dictate how rollout, etc, function
