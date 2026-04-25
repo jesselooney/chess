fe65874dcc4de1ca8d103c6a7da3a6fd58e787e1
Allison K <amklinglerr@gmail.com>
2026-04-24 22:50:25 -0400

    trying to make mcts progress

--- chess_engine/mcts.py
+++ chess_engine/mcts.py
@@ -1,5 +1,16 @@
 # mcts: selection -> expansion -> simulation -> backpropagation
+import random
+import chess
+from chess import Board, Move
 
+piece_values: dict[chess.PieceType, int] = {
+    chess.PAWN: 1,
+    chess.BISHOP: 3,
+    chess.KNIGHT: 3,
+    chess.ROOK: 5,
+    chess.QUEEN: 9,
+    chess.KING: 100,
+}
 
 class State:
     def __init__(self, value, board, player):
@@ -27,11 +38,22 @@ class State:
             else:
                 return 0
             # determine which player won the game? agent or opponent...
+        else:
+            total_value: int = 0
+            for piece in self.board.piece_map().values():
+                val = piece_values.get(piece.piece_type, 0)
 
+                if piece.color == self.player:
+                    total_value += val
+                else:
+                    total_value -= val
+            return total_value
         # the rest tbd for the recursive steps i sppose
 
     def next_state(self, move):
-        pass
+        # do we have to only do next state when it is our turn? i think that is handled by the tree imo...
+        next = State(self.value, self.board.push(move), self.player)
+        return next
 
 
 # keep track of visited nodes so we don't loop infinitely
@@ -57,15 +79,32 @@ class Node:
         if len(self.untried_actions) == 0:
             return True
         return False
+    
+    def expand(self):
+        pass
 
     def rollout(self):
-        # based on rollout policy -> tbd
-        pass
+        # random rollout policy for now
+        current_state = self.state
+        while not current_state.is_terminal():
+            current_state = current_state.next_state(random.choice(current_state.moves))
+        return current_state.reward()
 
     def backpropagate(self):
         pass
 
     def best_child(self):
-        pass
+        best_score = 0.0
+        best_child = []
+        for child in self.children:
+            score = child.reward / child.visited  # or some other scoring function?
+            if score == best_score:
+                best_child.append(child)
+            if score > best_score:
+                best_score = score
+                best_child = [child]
+        return random.choice(best_child)
+    # using bestness polcy or random rollout policy?? not entirely sure how these two interact
+    # i think we need to have the rollouts simulated here or perhaps when we expand
 
     # non-class based rollout policy and tree policy or do those go in here to dictate how rollout, etc, function
