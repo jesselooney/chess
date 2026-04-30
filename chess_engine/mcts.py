575626203f35f60da031cefe5f05673cd30d173e
Jesse Looney <jesselooney+dev@pm.me>
2026-04-29 21:08:31 -0400

    write MCTS

--- chess_engine/mcts.py
+++ chess_engine/mcts.py
@@ -1,110 +1,136 @@
-# mcts: selection -> expansion -> simulation -> backpropagation
+from dataclasses import dataclass
+import math
 import random
-import chess
+
 from chess import Board, Move
 
-piece_values: dict[chess.PieceType, int] = {
-    chess.PAWN: 1,
-    chess.BISHOP: 3,
-    chess.KNIGHT: 3,
-    chess.ROOK: 5,
-    chess.QUEEN: 9,
-    chess.KING: 100,
-}
-
-class State:
-    def __init__(self, value, board, player):
-        self.value = value
-        self.moves = board.legal_moves
-        self.board = board
-        self.player = player  # this is whether we are black or white
-        # something to keep track of whether it is this agents turn or not?
-
-    def is_terminal(self):
-        if self.board.is_checkmate():
-            return True
-        if self.board.is_stalemate():
-            return True
-        if len(self.moves) == 0:
-            return True
-        return False
-
-    def reward(self):
-        if self.board.is_stalemate():
-            return 50
-        if self.board.is_checkmate():
-            if self.board.outcome().winner == self.player:
-                return 100
-            else:
-                return 0
-            # determine which player won the game? agent or opponent...
-        else:
-            total_value: int = 0
-            for piece in self.board.piece_map().values():
-                val = piece_values.get(piece.piece_type, 0)
-
-                if piece.color == self.player:
-                    total_value += val
-                else:
-                    total_value -= val
-            return total_value
-        # the rest tbd for the recursive steps i sppose
-
-    def next_state(self, move):
-        # do we have to only do next state when it is our turn? i think that is handled by the tree imo...
-        next = State(self.value, self.board.push(move), self.player)
-        return next
+from chess_engine.evaluations import Evaluator
+from chess_engine.player import Player
 
 
-# keep track of visited nodes so we don't loop infinitely
+@dataclass
 class Node:
-    def __init__(self, state, parent=None):
-        self.visited = False  # or does the visited-ness tracker go in states?
-        self.reward = 0.0
-        self.state = state
-        self.children = []
-        self.parent = parent
-        self.untried_actions = (
-            self.state.moves
-        )  # need to copy this because we will be adding/removing?
+    board: Board
+    parent: Node | None
+    last_move: Move | None
+    children: list[Node]
+    unexplored_moves: list[Move]
+    visit_count: int
+    total_reward: float
 
-    def add_child(self, child_state):
-        child = Node(child_state, self)
+    @classmethod
+    def new(self, board: Board):
+        return Node(
+            board=board,
+            parent=None,
+            last_move=None,
+            children=[],
+            unexplored_moves=list(board.legal_moves),
+            visit_count=0,
+            total_reward=0,
+        )
+
+    def explore_move(self, move: Move) -> Node:
+        new_board = self.board.copy()
+        new_board.push(move)
+
+        child = Node(
+            board=new_board,
+            parent=self,
+            last_move=move,
+            children=[],
+            unexplored_moves=list(new_board.legal_moves),
+            visit_count=0,
+            total_reward=0,
+        )
+
+        self.unexplored_moves.remove(move)
         self.children.append(child)
 
-    def update(self, reward):
-        self.reward += reward
+        return child
 
-    def is_expanded(self):
-        if len(self.untried_actions) == 0:
-            return True
-        return False
-    
-    def expand(self):
-        pass
+    def observe_reward(self, reward: float):
+        self.total_reward += reward
+        self.visit_count += 1
 
-    def rollout(self):
-        # random rollout policy for now
-        current_state = self.state
-        while not current_state.is_terminal():
-            current_state = current_state.next_state(random.choice(current_state.moves))
-        return current_state.reward()
 
-    def backpropagate(self):
-        pass
+def ucb1(parent: Node, child: Node, c: float) -> float:
+    return (child.total_reward / child.visit_count) + c * math.sqrt(
+        2 * math.log(parent.visit_count) / child.visit_count
+    )
 
-    def best_child(self):
-        best_score = 0.0
-        best_child = []
-        for child in self.children:
-            score = child.reward / child.visited  # or some other scoring function?
-            if score == best_score:
-                best_child.append(child)
-            if score > best_score:
-                best_score = score
-                best_child = [child]
-        return random.choice(best_child)
-    # using bestness polcy or random rollout policy?? not entirely sure how these two interact
-    # i think we need to have the rollouts simulated here or perhaps when we expand
 
-    # non-class based rollout policy and tree policy or do those go in here to dictate how rollout, etc, function
+class MCTSPlayer(Player):
+    def __init__(
+        self,
+        evaluator: Evaluator,
+        rollout_count: int,
+        rollout_max_depth: int | None = None,
+    ):
+        super().__init__()
+
+        self.evaluator: Evaluator = evaluator
+
+        self.rollout_count: int = rollout_count
+
+        if rollout_max_depth is None:
+            self.rollout_max_depth: float = math.inf
+        else:
+            assert rollout_max_depth >= 0
+            self.rollout_max_depth: float = rollout_max_depth
+
+    def decide_move(self, board: Board) -> Move | None:
+        # TODO: Preserve part of the tree across calls?
+        root = Node.new(board)
+        for _ in range(self.rollout_count):
+            node = self.tree_policy(root)
+            reward = self.rollout(node.board.copy())
+            self.back_propagate(node, reward)
+        return self.best_child(root).last_move
+
+    def tree_policy(self, node: Node) -> Node:
+        while node.unexplored_moves or node.children:
+            if node.unexplored_moves:
+                return self.expand_node(node)
+            else:
+                node = self.best_child(node)
+        return node
+
+    def expand_node(self, node: Node) -> Node:
+        # TODO: How to select an action?
+        move = random.choice(node.unexplored_moves)
+        return node.explore_move(move)
+
+    def best_child(self, parent: Node) -> Node:
+        # TODO: Factor out the child heuristic as a parameter.
+        def heuristic(child: Node) -> float:
+            # We use the constant suggested in Browne et al. "A Survey of MCTS Methods"
+            c = 1 / math.sqrt(2)
+            return ucb1(parent, child, c)
+
+        # TODO: How to break ties?
+        return max(parent.children, key=heuristic)
+
+    def rollout(self, board: Board) -> float:
+        # TODO: Factor out rollout policy as a parameter.
+        my_color = board.turn
+
+        depth = 0
+        while (moves := list(board.legal_moves)) and depth < self.rollout_max_depth:
+            move = random.choice(moves)
+            board.push(move)
+            depth += 1
+
+        evaluation = self.evaluator.evaluate(board)
+
+        if board.turn != my_color:
+            evaluation *= -1
+
+        return evaluation
+
+    def back_propagate(self, node: Node, reward: float):
+        node: Node | None = node
+        while node is not None:
+            node.observe_reward(reward)
+            reward = -reward
+            node = node.parent
